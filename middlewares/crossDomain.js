6b806f4f74e1cd7dacb9359e1dd1bc765e35fb19
Ali Waseem <waseema393@gmail.com>
2016-01-21 11:47:33 -0700

    added eslint support

--- middlewares/crossDomain.js
+++ middlewares/crossDomain.js
@@ -2,12 +2,12 @@
 let allowCrossDomain = function(req, res, next) {
 
     // Right now I allow any website to talk to this API, this might need to change.....
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
 
     // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
+    if ("OPTIONS" == req.method) {
         res.sendStatus(200);
     }
     else {
